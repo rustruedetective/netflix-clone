f413f30102fb7bcd84fd8b590ef1fc707fdebc9a
Abbas <rustruedetective@gmail.com>
2022-07-04 10:23:31 -0700

    major request changes

--- src/components/Row/Poster.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Poster({ name, posterSrc, size }) {
-  return (
-    <div style={{ height: `${size.y}vh`, width: `${size.x}vw` }}>{name}</div>
-  );
-}
-
-export default Poster;
