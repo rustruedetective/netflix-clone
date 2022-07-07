c5474843ccb4f100ee2e7973adc0d8c4878daf66
Abbas <rustruedetective@gmail.com>
2022-07-07 02:19:54 -0700

    completed surprise

--- src/components/SurpriseMe/SurpriseMe.js
+++ src/components/SurpriseMe/SurpriseMe.js
@@ -4,12 +4,12 @@ import { BsChevronRight, BsChevronLeft } from "react-icons/bs";
 import { MdKeyboardArrowUp } from "react-icons/md";
 
 import Trailer from "../Trailer/Trailer";
+import Details from "../Details/Details";
 import "./styles/styles.css";
 
 function SurpriseMe({ data }) {
   const ref = useRef(null);
   const [index, setIndex] = useState(0);
-  console.log(data?.row?.[index]);
 
   const incWall = (_) => {
     if (index < data?.row?.length - 1) setIndex(index + 1);
@@ -18,16 +18,22 @@ function SurpriseMe({ data }) {
     if (index > 0) setIndex(index - 1);
   };
 
-  useEffect((_) => {
-    let transition1 = null;
-    let transition2 = null;
-    let transition3 = null;
+  const transition1 = () => {
+    ref.current.style.transition = "opacity 1s ease-out";
+    ref.current.style.opacity = 1;
+  };
 
-    const handler = () => {};
+  useEffect(
+    (_) => {
+      let timer1 = null;
+      ref.current.style.transition = "opacity 0s ease-out";
+      ref.current.style.opacity = 0;
 
-    transition1 = setTimeout(handler, 2000);
-    return (_) => clearTimeout(transition1);
-  }, []);
+      timer1 = setTimeout(transition1, 2000);
+      return (_) => clearTimeout(timer1);
+    },
+    [index]
+  );
 
   return (
     <div className="surprise">
@@ -59,6 +65,17 @@ function SurpriseMe({ data }) {
                   <span className="exit-text">Exit</span>
                 </Link>
               </div>
+            </div>
+            <div className="trailer-details" ref={ref}>
+              <Details
+                textAlign="left"
+                width="40vw"
+                fontSize="20px"
+                original={false}
+                name={data?.row?.[index]?.name}
+                description=""
+                categories={data?.row?.[index]?.genres}
+              />
             </div>
           </div>
         </div>
