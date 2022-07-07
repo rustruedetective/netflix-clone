c5e86af907d02357fc513664ee0e4f3459dacfcf
Abbas <rustruedetective@gmail.com>
2022-07-07 01:42:19 -0700

    surprise style fix

--- src/components/SurpriseMe/SurpriseMe.js
+++ src/components/SurpriseMe/SurpriseMe.js
@@ -8,16 +8,22 @@ import "./styles/styles.css";
 
 function SurpriseMe({ data }) {
   const ref = useRef(null);
-  const [item, setItem] = useState(5);
+  const [index, setIndex] = useState(0);
+  console.log(data?.row?.[index]);
+
+  const incWall = (_) => {
+    if (index < data?.row?.length - 1) setIndex(index + 1);
+  };
+  const decWall = (_) => {
+    if (index > 0) setIndex(index - 1);
+  };
 
   useEffect((_) => {
     let transition1 = null;
     let transition2 = null;
     let transition3 = null;
 
-    const handler = () => {
-      console.log(1);
-    };
+    const handler = () => {};
 
     transition1 = setTimeout(handler, 2000);
     return (_) => clearTimeout(transition1);
@@ -26,9 +32,21 @@ function SurpriseMe({ data }) {
   return (
     <div className="surprise">
       <div className="colorWrapper">
-        <div className="colorBlack"></div>
+        <div className="colorBlack">
+          <div
+            className="colorBlack-a"
+            style={{ background: index === 0 ? "transparent" : "black" }}
+          ></div>
+          <div
+            className="colorBlack-b"
+            style={{
+              background:
+                index === data?.row?.length - 1 ? "transparent" : "black",
+            }}
+          ></div>
+        </div>
         <div className="surprise-trailer">
-          <Trailer wall={data?.row?.[item]?.wall} />
+          <Trailer wall={data?.row?.[index]?.wall} />
           <div className="trailer-content">
             <div className="exit-hover-wrapper">
               <div className="exit">
@@ -45,19 +63,25 @@ function SurpriseMe({ data }) {
           </div>
         </div>
 
-        <div className="surprise-interactible-left">
-          <span className="icon-span">
-            <BsChevronLeft className="l-icon" />
-          </span>
-          <span className="left"></span>
-        </div>
+        {index === 0 ? (
+          ""
+        ) : (
+          <div className="surprise-interactible-left">
+            <span className="icon-span" onClick={decWall}>
+              <BsChevronLeft className="l-icon" />
+            </span>
+          </div>
+        )}
 
-        <div className="surprise-interactible-right">
-          <span className="right"></span>
-          <span className="icon-span">
-            <BsChevronRight className="r-icon" />
-          </span>
-        </div>
+        {index === data?.row?.length - 1 ? (
+          ""
+        ) : (
+          <div className="surprise-interactible-right">
+            <span className="icon-span" onClick={incWall}>
+              <BsChevronRight className="r-icon" />
+            </span>
+          </div>
+        )}
       </div>
     </div>
   );
